9e825d26d32edca3efe7d6a3971a4701db2f63a1
Ahmad Moltafet <moltafet.ahmad2015@gmail.com>
2023-04-05 15:57:22 -0700

    g

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -11,13 +11,38 @@ import Button from '@mui/joy/Button';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 function MenuBar() {
+
+  const backBtn = () => {
+    window.history.back();
+  };
+
+  const backBtnDisplay = () => {
+    if (window.location.pathname === "/") {
+      return (
+        <> </>
+      )
+    } else {
+      return (
+        <div style={{
+          position: "absolute",
+          left: "3%",
+          marginTop: "1%",
+        }}>
+      <Button aria-label="Like" variant="outlined" color="neutral" sx={{marginRight: "5px"}} onClick={
+        backBtn
+      }> <KeyboardArrowLeftIcon fontSize="small"/> Back</Button>
+      </div>
+      )
+    }
+    }
 
   return (
     <AppBar >
       <CssBaseline />
+          {backBtnDisplay()}
           <div style={{
             position: "absolute",
             right: "3%",
@@ -33,6 +58,7 @@ function MenuBar() {
           <Button aria-label="Like" variant="outlined" color="neutral">
             <SettingsOutlinedIcon />
           </Button>
+  
            
           </div>
     </AppBar>
