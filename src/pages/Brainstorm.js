70c8763432be3469a7fd90801ac96c7dcc834c7b
Mason Manca <masonmanca@Masons-MacBook-Pro-2.local>
2023-04-11 18:35:40 -0700

    brainstorm page, figuring out mui:
    ;

--- src/pages/Brainstorm.js
+++ src/pages/Brainstorm.js
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
 import { Button, Card, Grid, Typography } from "@mui/material";
-import Input from '@mui/joy/Input';
-import TextFieldsIcon from '@mui/icons-material/TextFields';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import IconButton from '@mui/joy/IconButton';
-import Tooltip from '@mui/joy/Tooltip';
-import Alert from '@mui/joy/Alert';
-import PlaylistAddCheckCircleRoundedIcon from '@mui/icons-material/PlaylistAddCheckCircleRounded';
 import MenuBar from "../components/MenuBar";
 import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
@@ -36,8 +29,8 @@ function Brainstorm () {
               marginLeft: "10%",
               marginRight: "10%",
             }}>
-<Box sx={{ border: '1px solid black', display: 'flex', flexDirection: 'column', height: '30%', width: '100%'}}>
-  <Box sx={{ display: 'flex', alignItems: 'center', px: 2, py: 1, m:1 }}>
+<Box sx={{ border: '1px solid rgba(0, 0, 0, 0.5)', display: 'flex', flexDirection: 'column', height: '30%', width: '100%', mb:1}}>
+  <Box sx={{ display: 'flex', alignItems: 'center', px: 2, py: 0, m:1, mb:-1 }}>
     <Typography variant="h5" component="span" sx={{ fontWeight: 'bold' }}>
       Try
     </Typography>
@@ -45,16 +38,16 @@ function Brainstorm () {
   <Box sx={{ p: 2 }}>
     <Grid container spacing={2}>
       <Grid item xs={4}>
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid black', p: 2  }}>Filler</Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid black', m:2, p:2,borderRadius:'16px'  }}>Filler</Box>
       </Grid>
       <Grid item xs={4}>
-        <Box sx={{ justifyContent:'center', alignItems: 'center', border: '1px solid black', p: 2 }} >Filler </Box>
+        <Box sx={{ justifyContent:'center', alignItems: 'center', border: '1px solid black', m:2, p:2,borderRadius:'16px' }} >Filler </Box>
       </Grid>
       <Grid item xs={4}>
-        <Box sx={{ justifyContent:'center', alignItems: 'center', border: '1px solid black', p: 2 }} >Filler </Box>
+        <Box sx={{ justifyContent:'center', alignItems: 'center', border: '1px solid black', m:2, p:2,borderRadius:'16px' }} >Filler </Box>
       </Grid>
       <Grid item xs={12}>
-        <Typography variant="body1" contentEditable onInput={handleInput}sx={{border: '1px solid black'}}>
+        <Typography variant="body1" contentEditable onInput={handleInput}sx={{border: '1px solid black', m:2, p:1, borderRadius:'16px'}}>
           Type Here
         </Typography>
       </Grid>
@@ -62,26 +55,24 @@ function Brainstorm () {
   </Box>
 </Box>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <Button variant="contained" sx={{ height: 25, w: 40, m: 1, alignSelf: 'flex-start' }}>Old Chats</Button>
+      <Button variant="contained" sx={{ height: 25, w: 40, mt:1, mb:3, alignSelf: 'flex-start' }}>Old Chats</Button>
       </div>
-      <Card sx={{display:'flex', minWidth: 300}}>
+      <Card sx={{display:'flex', minWidth: 300, mt:1}}>
         <CardContent>
-          <Button variant="contained" sx={{ height: 30, w: 40, m: 1, alignSelf:'flex-end',justifySelf:'flex-end'}}>Search</Button>
-
-            <List >
-                <ListItem>
-                <ListItemText variant ='h5' sx={{}}>How to write an http request in Java </ListItemText>
-                </ListItem> 
+          <Button variant="contained" sx={{ height: 25, w: 40}}>Search</Button>
+            <List>
+              <ListItem><ListItemText variant ='h5' sx={{}}>How to write an http request in Java? </ListItemText></ListItem>  
+              <ListItem><ListItemText variant ='h5' sx={{}}>What python libraries would help my project? </ListItemText></ListItem>  
+              <ListItem><ListItemText variant ='h5' sx={{}}>What are stars made of? </ListItemText></ListItem>  
             </List>
         </CardContent>
       </Card>
-      <Card sx={{minWidth: 300}}>
+      <Card sx={{minWidth: 300, mt:1, mb:1}}>
       <CardContent sx={{display: 'flex'}}>
             <List >
-                <ListItem>
-                  <ListItemText></ListItemText>
-
-                </ListItem>
+              <ListItem><ListItemText variant ='h5' sx={{}}>Light Mode</ListItemText></ListItem>
+              <ListItem><ListItemText variant ='h5' sx={{}}>Updates and FAQ</ListItemText></ListItem> 
+              <ListItem><ListItemText variant ='h5' sx={{}}>Clear all Conversations</ListItemText></ListItem>
             </List>
         </CardContent>
       </Card>
